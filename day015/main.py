e1233ec7a3aa4cfb51aa03d9503be6492efbf2b2
Sinoop Joy <joysinoop@gmail.com>
2021-02-22 22:36:39 -0500

    Day 15

--- day015/main.py
+++ day015/main.py
@@ -2,7 +2,7 @@ from cofee_machine import CoffeeMachine
 from resources import MENU
 
 
-def RepresentsInt(s):
+def represents_int(s):
     try:
         int(s)
         return True
@@ -20,15 +20,15 @@ while True:
     for i in range(0, len(valid_items)):
         print(f"{i + 1}.{valid_items[i]} (${MENU.get(valid_items[i]).get('cost')})")
     choice = input(f"What would you like ? :")
-    if choice in valid_items or (RepresentsInt(choice) and int(choice) in range(1, len(valid_items) + 1)):
-        if RepresentsInt(choice):
+    if choice in valid_items or (represents_int(choice) and int(choice) in range(1, len(valid_items) + 1)):
+        if represents_int(choice):
             item = valid_items[int(choice) - 1]
         else:
             item = choice
 
         if coffe_machine_instance.check_if_sufficient_ingredients_present(item):
             coffe_machine_instance.make_item(item)
-    elif choice == 'report':
+    elif choice == 'report' or (represents_int(choice) and int(choice) == -1):
         coffe_machine_instance.print_ingredients_report()
         coffe_machine_instance.print_cash_balance_report()
     else:
